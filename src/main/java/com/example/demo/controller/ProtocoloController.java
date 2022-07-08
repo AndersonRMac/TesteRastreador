aeff4b7e24716ebd1696ec388427482ab2ef3e5b
AndersonRMac <anderson.ramosmaciel@gmail.com>
2022-07-08 08:53:13 -0300

    08/07/2022

--- src/main/java/com/example/demo/controller/ProtocoloController.java
+++ src/main/java/com/example/demo/controller/ProtocoloController.java
@@ -2,18 +2,28 @@ package com.example.demo.controller;
 
 import com.example.demo.service.TesteProtocolo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.model.Ultimo_dado_lido;
 import com.example.demo.repository.Ult_dado_lidoRepository;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping("/")
+@Controller
+@RequestMapping(value = "/")
 public class ProtocoloController {
 
     @Autowired
     Ult_dado_lidoRepository dao;
 
     TesteProtocolo testeProtocolo = new TesteProtocolo();
+
+    @GetMapping(value = "/principal")
+    public ModelAndView pagina (){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("home/index");
+        return mv;
+    }
 
     @PostMapping
     public String post(String name){
