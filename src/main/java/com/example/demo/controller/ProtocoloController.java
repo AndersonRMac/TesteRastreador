e6717c0da383e19670da269d48deb4afdde2caa5
AndersonRMac <anderson.ramosmaciel@gmail.com>
2022-07-11 17:22:08 -0300

    11/07/2022

--- src/main/java/com/example/demo/controller/ProtocoloController.java
+++ src/main/java/com/example/demo/controller/ProtocoloController.java
@@ -2,20 +2,27 @@ package com.example.demo.controller;
 
 import com.example.demo.service.TesteProtocolo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import com.example.demo.model.Ultimo_dado_lido;
 import com.example.demo.repository.Ult_dado_lidoRepository;
 
 
-@Controller
-@RequestMapping(value = "/")
+@RestController
 public class ProtocoloController {
 
     @Autowired
     Ult_dado_lidoRepository dao;
 
     TesteProtocolo testeProtocolo = new TesteProtocolo();
+
+    @RequestMapping( value = "/create1", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String create1(){
+        return "create";
+    }
+
 
     @PostMapping
     public String post(String name){
